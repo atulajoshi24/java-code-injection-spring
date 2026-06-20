b9acf9f54ee056946345067cba778ab8f56a942d
atulajoshi24 <user@localhost.localdomain>
2026-06-20 14:35:38 +0530

    code enahnced to include java script engine use case

--- src/main/java/com/securecode/ssrf/UserController.java
+++ src/main/java/com/securecode/ssrf/UserController.java
@@ -2,7 +2,14 @@ package com.securecode.ssrf;
 
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -21,5 +28,17 @@ public class UserController {
         model.addAttribute("userInput", header);
         return "safe_template";
     }
+	
+	@GetMapping("/calculate")
+	@ResponseBody
+	public String calculate(@RequestHeader(value = "Injected-Header", defaultValue = "") String expression) throws Exception {
+	    ScriptEngineManager manager = new ScriptEngineManager();
+	    ScriptEngine engine = manager.getEngineByName("JavaScript");
+
+	    Object result = engine.eval(expression);
+	    System.out.println("result --> "+result);
+	    return result.toString();
+
+	}
 
 }
